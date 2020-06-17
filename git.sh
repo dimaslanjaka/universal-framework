a338026000f789bdcbf659440f243538129e9a0f
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-17 21:45:25 +0700

    fc

---
+++ git.sh
@@ -0,0 +1,20 @@
+cd D:/dimaslanjaka.github.io/php-yt-api
+git add views/index.obfuscated.js
+git commit -m Update views/index.obfuscated.js
+git add views/index.obfuscated.min.js
+git commit -m Update views/index.obfuscated.min.js
+git add views/signin.obfuscated.js
+git commit -m Update views/signin.obfuscated.js
+git add views/signin.obfuscated.min.js
+git commit -m Update views/signin.obfuscated.min.js
+git add views/superuser/php/index.obfuscated.js
+git commit -m Update views/superuser/php/index.obfuscated.js
+git add views/superuser/php/index.obfuscated.min.js
+git commit -m Update views/superuser/php/index.obfuscated.min.js
+git add views/tools/json-viewer/index.obfuscated.js
+git commit -m Update views/tools/json-viewer/index.obfuscated.js
+git add views/tools/json-viewer/index.obfuscated.min.js
+git commit -m Update views/tools/json-viewer/index.obfuscated.min.js
+
+
+rm -- "$0"
