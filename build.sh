fc21036fe641c720f6fba0f634e923f24ec00075
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-17 22:56:48 +0700

    Update

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ RED='\033[0;41;30m'
 STD='\033[0;0;39m'
 
 node libs/compiler/git.js
-read -p "Press [Enter] key to continue..." fackEnterKey
+#read -p "Press [Enter] key to continue..." fackEnterKey
 
 :'
 pause() {
