2ff3d54fa5b5968a4d186664dccd34abc70270e1
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-17 23:06:31 +0700

    Update

--- git.sh
+++ git.sh
@@ -1,8 +1,32 @@
 cd D:/dimaslanjaka.github.io/php-yt-api
+git add config.json
+git commit -m Update config.json
+git add git.sh
+git commit -m Update git.sh
+git add libs/compiler/single.js
+git commit -m Update libs/compiler/single.js
+git add views/css/bg.css
+git commit -m Update views/css/bg.css
+git add views/css/bg.min.css
+git commit -m Update views/css/bg.min.css
+git add views/css/gradient.css
+git commit -m Update views/css/gradient.css
+git add views/css/square.css
+git commit -m Update views/css/square.css
+git add views/css/square.min.css
+git commit -m Update views/css/square.min.css
+git add views/img/unsplash.php
+git commit -m Update views/img/unsplash.php
+git add views/index.css
+git commit -m Update views/index.css
+git add views/index.min.css
+git commit -m Update views/index.min.css
 git add views/index.obfuscated.js
 git commit -m Update views/index.obfuscated.js
 git add views/index.obfuscated.min.js
 git commit -m Update views/index.obfuscated.min.js
+git add views/index.scss
+git commit -m Update views/index.scss
 git add views/signin.obfuscated.js
 git commit -m Update views/signin.obfuscated.js
 git add views/signin.obfuscated.min.js
