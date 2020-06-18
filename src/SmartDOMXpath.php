7074fe337ddd6a099d411e27af4250dd8b69ed40
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-18 18:18:17 +0700

    Update src/SmartDOMXpath.php

--- src/SmartDOMXpath.php
+++ src/SmartDOMXpath.php
@@ -1,22 +1,13 @@
 <?php
 
+/**
+ * Query selector
+ *  @method \DOMNodeList[]|\DOMNode[]|\DOMElement[] query(string $expression, \DOMNode $contextnode, boolean $registerNodeNS)
+ */
 class SmartDOMXpath extends DOMXpath
 {
   public function __construct(\DOMDocument $dom)
   {
     parent::__construct($dom);
-  }
-
-  /**
-   * Query selector
-   *
-   * @param string $expression
-   * @param boolean $contextnode
-   * @param boolean $registerNodeNS
-   * @return \DOMNodeList[]|\DOMNode[]|\DOMElement[]
-   */
-  function query($expression, $contextnode = null, $registerNodeNS = true)
-  {
-    return parent::query($expression, $contextnode, $registerNodeNS);
   }
 }
