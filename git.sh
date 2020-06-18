ec364520783f55c386e1913613ba4ded6a80c5ea
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-18 08:29:33 +0700

    Update

--- git.sh
+++ git.sh
@@ -1,8 +1,42 @@
 cd D:/dimaslanjaka.github.io/php-yt-api
+git add composer.json
+git commit -m Update composer.json
 git add config.json
 git commit -m Update config.json
 git add git.sh
 git commit -m Update git.sh
+git add index.php
+git commit -m Update index.php
+git add libs/bin/php-cs-fixer
+git commit -m Update libs/bin/php-cs-fixer
+git add libs/sftp/config.js
+git commit -m Update libs/sftp/config.js
+git add libs/sftp/connect.js
+git commit -m Update libs/sftp/connect.js
+git add libs/sftp/index.js
+git commit -m Update libs/sftp/index.js
+git add libs/sftp/index.php
+git commit -m Update libs/sftp/index.php
+git add libs/sftp/list.css
+git commit -m Update libs/sftp/list.css
+git add libs/sftp/list.php
+git commit -m Update libs/sftp/list.php
+git add libs/sftp/local.js
+git commit -m Update libs/sftp/local.js
+git add package.json
+git commit -m Update package.json
+git add src/bootstrap.php
+git commit -m Update src/bootstrap.php
+git add src/globals.php
+git commit -m Update src/globals.php
+git add src/JSLikeHTMLElement.php
+git commit -m Update src/JSLikeHTMLElement.php
+git add src/MVC/themes/mdb-dashboard/script.php
+git commit -m Update src/MVC/themes/mdb-dashboard/script.php
+git add src/SmartDOMDocument.php
+git commit -m Update src/SmartDOMDocument.php
+git add src/SmartDOMXpath.php
+git commit -m Update src/SmartDOMXpath.php
 git add views/css/bg.css
 git commit -m Update views/css/bg.css
 git add views/css/bg.min.css
@@ -15,6 +49,8 @@ git add views/signin.obfuscated.js
 git commit -m Update views/signin.obfuscated.js
 git add views/signin.obfuscated.min.js
 git commit -m Update views/signin.obfuscated.min.js
+git add views/snippets/php/ping.php
+git commit -m Update views/snippets/php/ping.php
 git add views/superuser/breadcrumb.php
 git commit -m Update views/superuser/breadcrumb.php
 git add views/superuser/php/index.obfuscated.js
@@ -25,6 +61,8 @@ git add views/tools/json-viewer/index.obfuscated.js
 git commit -m Update views/tools/json-viewer/index.obfuscated.js
 git add views/tools/json-viewer/index.obfuscated.min.js
 git commit -m Update views/tools/json-viewer/index.obfuscated.min.js
+git add webpack.config.js
+git commit -m Update webpack.config.js
 
 
 rm -- "$0"
