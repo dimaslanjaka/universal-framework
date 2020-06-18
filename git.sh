96dc77d6f29c73674f6e5c9299bf39106460754e
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-18 12:31:48 +0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,8 +1,44 @@
 cd D:/dimaslanjaka.github.io/php-yt-api
 git add config.json
-git commit -m Update config.json
+git commit -m "Update config.json"
 git add git.sh
-git commit -m Update git.sh
+git commit -m "Update git.sh"
+git add index.php
+git commit -m "Update index.php"
+git add libs/compiler/git.js
+git commit -m "Update libs/compiler/git.js"
+git add libs/js/user.ts
+git commit -m "Update libs/js/user.ts"
+git add libs/js/utility.js
+git commit -m "Update libs/js/utility.js"
+git add src/MVC/themes/assets/js/app.d.ts
+git commit -m "Update src/MVC/themes/assets/js/app.d.ts"
+git add src/MVC/themes/assets/js/app.js
+git commit -m "Update src/MVC/themes/assets/js/app.js"
+git add src/MVC/themes/assets/js/app.js.map
+git commit -m "Update src/MVC/themes/assets/js/app.js.map"
+git add src/MVC/themes/assets/js/app.min.js
+git commit -m "Update src/MVC/themes/assets/js/app.min.js"
+git add views/css/bg.css
+git commit -m "Update views/css/bg.css"
+git add views/css/bg.min.css
+git commit -m "Update views/css/bg.min.css"
+git add views/index.obfuscated.js
+git commit -m "Update views/index.obfuscated.js"
+git add views/index.obfuscated.min.js
+git commit -m "Update views/index.obfuscated.min.js"
+git add views/signin.obfuscated.js
+git commit -m "Update views/signin.obfuscated.js"
+git add views/signin.obfuscated.min.js
+git commit -m "Update views/signin.obfuscated.min.js"
+git add views/superuser/php/index.obfuscated.js
+git commit -m "Update views/superuser/php/index.obfuscated.js"
+git add views/superuser/php/index.obfuscated.min.js
+git commit -m "Update views/superuser/php/index.obfuscated.min.js"
+git add views/tools/json-viewer/index.obfuscated.js
+git commit -m "Update views/tools/json-viewer/index.obfuscated.js"
+git add views/tools/json-viewer/index.obfuscated.min.js
+git commit -m "Update views/tools/json-viewer/index.obfuscated.min.js"
 
 
 rm -- "$0"
