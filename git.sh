422fa4b7721e9c4d07696ddb48e5925205f7e565
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-18 08:46:02 +0700

    Update

--- git.sh
+++ git.sh
@@ -1,6 +1,8 @@
 cd D:/dimaslanjaka.github.io/php-yt-api
 git add build.sh
 git commit -m Update build.sh
+git add config.json
+git commit -m Update config.json
 git add git.sh
 git commit -m Update git.sh
 
