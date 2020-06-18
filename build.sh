7fa9d2ca59b272ba6dc07537baa9967a97e0bd3b
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-18 14:30:59 +0700

    Third Impact (Binaries dual OS [WIN|UNIX])

--- build.sh
+++ build.sh
@@ -1,42 +1,74 @@
-#!/bin/bash
+#!/bin/sh
+
+dir=$(
+    cd "${0%[/\\]*}" >/dev/null
+    pwd
+)
+
+if [ -d /proc/cygdrive ]; then
+    case $(which php) in
+    $(readlink -n /proc/cygdrive)/*)
+        # We are in Cygwin using Windows php, so the path must be translated
+        dir=$(cygpath -m "$dir")
+        ;;
+    esac
+fi
 
 EDITOR=vim
 PASSWD=/etc/passwd
 RED='\033[0;41;30m'
 STD='\033[0;0;39m'
 
-node libs/compiler/git.js
+clean_git_history() {
+    git checkout --orphan latest_branch
+    git add -A
+    git commit -am "Clean Commit"
+    git branch -D master
+    git branch -m master
+    git push -f origin master
+}
 
-:'
+update() {
+    composer update -o
+    npm update
+    types-installer install
+}
+
+backup() {
+    DATE=$(date +"%d-%b-%Y")
+    cd /root
+    tar --exclude='./htdocs/tmp' --exclude='./htdocs/src/Session/sessions' --exclude='./htdocs/vendor' --exclude='./htdocs/node_modules' --exclude='./htdocs/processed' -zcvf backup-$DATE.tgz /root/htdocs
+}
+
+install() {
+    npm install -g javascript-obfuscator typescript terser nodemon sass typac types-installer
+    npm install
+    composer install
+}
+
 pause() {
     read -p "Press [Enter] key to continue..." fackEnterKey
 }
 
-# function to display menus
 show_menus() {
     clear
     echo "~~~~~~~~~~~~~~~~~~~~~"
     echo "   Library builder   "
     echo "~~~~~~~~~~~~~~~~~~~~~"
     echo "1. Single compiler"
-    echo "2. Watch Compile JS src/MVC/themes/assets/js/{app.js,app.min.js}"
+    echo "2. clean git commit history (Required Big Internet Data)"
     echo "3. Exit"
 }
-# read input from the keyboard and take a action
-# invoke the one() when the user select 1 from the menu option.
-# invoke the two() when the user select 2 from the menu option.
-# Exit when user the user select 3 form the menu option.
+
 read_options() {
     local choice
     read -p "Enter choice [ 1 - 3] " choice
     case $choice in
     1)
-        node libs/compiler/single.js
-        read -p "Press [Enter] key to continue..."
+        node libs/compiler/git.js
         ;;
     2)
-        npm run index
-        read -p "Press [Enter] key to continue..."
+        clean_git_history
         ;;
     3) exit 0 ;;
     *) echo -e "${RED}Error...${STD}" && sleep 2 ;;
@@ -44,15 +76,14 @@ read_options() {
 }
 
 # ----------------------------------------------
-# Step #3: Trap CTRL+C, CTRL+Z and quit singles
+# Trap CTRL+C, CTRL+Z and quit singles
 # ----------------------------------------------
 trap '' SIGINT SIGQUIT SIGTSTP
 
 # -----------------------------------
 # Step #4: Main logic - infinite loop
 # ------------------------------------
-while true; do
+:'while true; do
     show_menus
     read_options
-done
-'
+done'
