8d6e7797d0da88d17b82bff5f4259dfea69e06f3
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-18 09:04:16 +0700

    Update

--- git.sh
+++ git.sh
@@ -1,26 +1,8 @@
 cd D:/dimaslanjaka.github.io/php-yt-api
+git add config.json
+git commit -m Update config.json
 git add git.sh
 git commit -m Update git.sh
-git add views/css/bg.css
-git commit -m Update views/css/bg.css
-git add views/css/bg.min.css
-git commit -m Update views/css/bg.min.css
-git add views/index.obfuscated.js
-git commit -m Update views/index.obfuscated.js
-git add views/index.obfuscated.min.js
-git commit -m Update views/index.obfuscated.min.js
-git add views/signin.obfuscated.js
-git commit -m Update views/signin.obfuscated.js
-git add views/signin.obfuscated.min.js
-git commit -m Update views/signin.obfuscated.min.js
-git add views/superuser/php/index.obfuscated.js
-git commit -m Update views/superuser/php/index.obfuscated.js
-git add views/superuser/php/index.obfuscated.min.js
-git commit -m Update views/superuser/php/index.obfuscated.min.js
-git add views/tools/json-viewer/index.obfuscated.js
-git commit -m Update views/tools/json-viewer/index.obfuscated.js
-git add views/tools/json-viewer/index.obfuscated.min.js
-git commit -m Update views/tools/json-viewer/index.obfuscated.min.js
 
 
 rm -- "$0"
