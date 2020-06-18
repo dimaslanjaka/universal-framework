be68ae3899d122ea521e50e3659133f55eefacc4
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-18 14:54:01 +0700

    Merged

--- build.sh
+++ build.sh
@@ -46,6 +46,21 @@ install() {
     composer install
 }
 
+refresh() {
+    sync
+    if [ $(dpkg-query -W -f='${Status}' polipo 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+        apt-get install polipo -y
+    fi
+    polipo -x
+    echo 3 >/proc/sys/vm/drop_caches
+    swapoff -a && swapon -a
+    printf '\n%s\n\n' 'Ram-cache and Swap Cleared'
+    if [ -f /opt/lampp/xampp ]; then
+        /opt/lampp/xampp restart
+    fi
+    free -h
+}
+
 pause() {
     read -p "Press [Enter] key to continue..." fackEnterKey
 }
