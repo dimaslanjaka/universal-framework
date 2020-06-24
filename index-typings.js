1b1534410bdbbce26319d37f9cd252a206286f2c
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-06-24 22:06:32 +0700

    add automation typings package finder

--- index-typings.js
+++ index-typings.js
@@ -1,44 +1,135 @@
-const fs = require("fs");
-const path = require("path");
-const upath = require("upath");
-
-const log = require("./libs/compiler/log");
-
-const core = require("./libs/compiler/core");
-const sorter = require("./libs/compiler/sorter").sorter;
-const ncp = require("ncp").ncp;
+const fs = require('fs');
+const upath = require('upath');
+const log = require('./libs/compiler/log');
+const filemanager = require('./libs/compiler/filemanager');
 //shared packages.json
-const root_pkg = require("./package.json");
+const root_pkg = require('./package.json');
 const packages = {};
-Object.assign(packages, root_pkg.dependencies, root_pkg.devDependencies);
-return;
-console.clear();
+const axios = require('axios');
 
+Object.assign(packages, root_pkg.dependencies, root_pkg.devDependencies);
+
+var TypesLists = [];
 for (const key in packages) {
   if (packages.hasOwnProperty(key)) {
-    //D:\dimaslanjaka.github.io\php-yt-api\node_modules\sweetalert\typings
-    const typings = `./node_modules/${key}/typings`;
-    const move_to = `./libs/typings/${key}`;
-    fs.exists(move_to, function (exists) {
-      if (!exists) {
-        fs.mkdir(move_to, { recursive: true }, function (err) {});
-      }
-      if (path.resolve(typings).trim().length) {
-        //log(path.resolve(typings), fs.existsSync(typings));
+    const version = packages[key];
+    if (!key.includes('@types')) {
+      TypesLists.push(key);
+      TypesLists = shuffle(TypesLists);
+    }
+  }
+}
 
-        fs.exists(typings, function (exists) {
-          if (exists) {
-            ncp.limit = 100;
+fetchTypes();
+var TypesFound = [];
 
-            ncp(typings, move_to, function (err) {
-              if (err) {
-                return log.log(log.error(err));
-              }
-              log.log(`${typings} ${move_to} done!`);
-            });
-          }
-        });
+/**
+ * fetch typescript
+ * @param {string[]} TypesLists
+ */
+function fetchTypes() {
+  if (TypesLists.length == 0) {
+    log.log(log.error('queue empty'));
+    if (TypesFound.length) {
+      log.log(log.success('get ready to install'));
+      const { exec } = require('child_process');
+      exec('npmi ' + TypesFound.join(' '));
+    }
+    return;
+  }
+
+  findTypes(TypesLists[0], function (success, package) {
+    if (success) {
+      TypesFound.push(package);
+    }
+
+    log.log(success, package, TypesLists.length);
+    TypesLists.shift();
+    fetchTypes(TypesLists);
+  });
+}
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
+/**
+ * find typescript
+ * @param {string} package
+ * @param {function(boolean,string)} callback callback(success,packagename)
+ */
+function findTypes(package, callback) {
+  var cache = upath.join(__dirname, `tmp/npm/packages/@types/${package}.json`);
+  filemanager.mkdir(upath.dirname(cache));
+
+  if (fs.existsSync(cache)) {
+    var data = fs.readFileSync(cache);
+    var processNow = processPackage(data);
+    processNow
+      .then(function (success) {
+        if (success) {
+          return true;
+        }
+      })
+      .catch(function () {
+        getPackage(package);
+      });
+  } else {
+    getPackage(package);
+  }
+
+  function processPackage(body) {
+    try {
+      const json = JSON.parse(body);
+      console.log(json);
+      if (json.hasOwnProperty('name') && json.hasOwnProperty('description')) {
+        fs.writeFileSync(cache, body);
+        if (typeof callback == 'function') {
+          callback(true, '@types/' + package);
+          return true;
+        }
       }
-    });
+    } catch (error) {
+      getPackage(package);
+    }
+  }
+
+  function getPackage(package) {
+    axios
+      .get('https://registry.npmjs.org/@types/' + package, {
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+          'Content-Type':
+            'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*',
+          Accept:
+            'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*',
+          Referer: 'https://registry.npmjs.org/',
+          Connection: 'keep-alive',
+        },
+        timeout: 60000,
+      })
+      .catch(function (error) {
+        log.log(log.error(error));
+        if (typeof callback == 'function') {
+          return callback(false, '@types/' + package);
+        }
+      })
+      .then(function (res) {
+        if (res && res.hasOwnProperty('data')) {
+          processPackage(res.data);
+        }
+      });
   }
 }
---
+++ libs/compiler/compiler/queue.js
@@ -0,0 +1,62 @@
+"use strict";
+/**
+ * Creates a new queue. A queue is a first-in-first-out (FIFO) data structure -
+ * items are added to the end of the queue and removed from the front.
+ * @example
+ * // create a new queue
+ * var queue = new Queue();
+ * // enqueue an item
+ * queue.enqueue('item');
+ * // dequeue an item
+ * var item = queue.dequeue();
+ * // get the item at the front of the queue
+ * var item = queue.peek();
+ * // determine the number of items in the queue
+ * var length = queue.getLength();
+ * // determine whether the queue is empty
+ * var isEmpty = queue.isEmpty();
+ */
+function Queue() {
+    // initialise the queue and offset
+    var queue = [];
+    var offset = 0;
+    // Returns the length of the queue.
+    this.getLength = function () {
+        return queue.length - offset;
+    };
+    // Returns true if the queue is empty, and false otherwise.
+    this.isEmpty = function () {
+        return queue.length == 0;
+    };
+    /* Enqueues the specified item. The parameter is:
+     *
+     * item - the item to enqueue
+     */
+    this.enqueue = function (item) {
+        queue.push(item);
+    };
+    /* Dequeues an item and returns it. If the queue is empty, the value
+     * 'undefined' is returned.
+     */
+    this.dequeue = function () {
+        // if the queue is empty, return immediately
+        if (queue.length == 0)
+            return undefined;
+        // store the item at the front of the queue
+        var item = queue[offset];
+        // increment the offset and remove the free space if necessary
+        if (++offset * 2 >= queue.length) {
+            queue = queue.slice(offset);
+            offset = 0;
+        }
+        // return the dequeued item
+        return item;
+    };
+    /* Returns the item at the front of the queue (without dequeuing it). If the
+     * queue is empty then undefined is returned.
+     */
+    this.peek = function () {
+        return queue.length > 0 ? queue[offset] : undefined;
+    };
+}
+module.exports = Queue;
