898ddb8e8400e6e005a8e13788281ceda55ee419
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-07-02 13:47:43 +0700

    fix [02072020 13:47]

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-var index = require("./src/node/index");
+//var index = require("./src/node/index");
+const tsd = require("tsd").;
+(async () => {
+  const diagnostics = await tsd();
+
+  console.log(diagnostics.length);
+  //=> 2
+})();
