f1b0bc8f898a56de096fd7bce1a8ae199fdb799d
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-07-04 20:43:34 +0700

    fix path

--- libs/src/syncs/sync.ts
+++ libs/src/syncs/sync.ts
@@ -1,12 +1,12 @@
-import Config from './Config';
-import * as upath from 'upath';
-import * as fs from 'fs';
-import * as process from 'process';
-import chokidar from 'chokidar';
-import observatory from './../observatory/lib/observatory';
-import { Client } from 'scp2';
-import { readFileSync } from 'fs';
-import sftp from './sftp';
+import Config from "./Config";
+import * as upath from "upath";
+import * as fs from "fs";
+import * as process from "process";
+import chokidar from "chokidar";
+import observatory from "../observatory/lib/observatory";
+import { Client } from "scp2";
+import { readFileSync } from "fs";
+import sftp from "./sftp";
 
 export default class sync {
   config: Config;
@@ -15,11 +15,11 @@ export default class sync {
   sftp: sftp;
 
   constructor() {
-    this.task = observatory.add('Initializing...');
+    this.task = observatory.add("Initializing...");
 
     // Get config
     this.config = new Config();
-    this.task.status('reading config');
+    this.task.status("reading config");
 
     this.config
       .ready()
@@ -27,25 +27,25 @@ export default class sync {
         // Get Command line interface
         //this.cli.write("Connecting");
         //this.cli.startProgress();
-        this.task.status('watching files');
+        this.task.status("watching files");
 
         var ori = this.config.localPath;
         var root = process.cwd();
         if (upath.isAbsolute(this.config.localPath)) {
           var mod = upath.normalizeSafe(
-            this.config.localPath.replace(root, '')
+            this.config.localPath.replace(root, "")
           );
           if (mod == ori) {
-            throw new Error('Cannot find absolute path');
+            throw new Error("Cannot find absolute path");
           } else {
-            this.config.localPath = '.' + mod;
+            this.config.localPath = "." + mod;
           }
         }
 
         this.config.localPath = upath.normalizeSafe(this.config.localPath);
         fs.exists(this.config.localPath, function (es) {
           if (!es) {
-            throw new Error('Local path not exists');
+            throw new Error("Local path not exists");
           }
         });
 
@@ -54,7 +54,7 @@ export default class sync {
         return true;
       })
       .then(async () => {
-        this.task.status('connecting server');
+        this.task.status("connecting server");
 
         const any = await this.sftp.connect();
         //this.task.done(any).details(this.config.host);
