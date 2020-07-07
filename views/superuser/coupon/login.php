1f2bcf44dc186d6470877b63cd57ec11fe44606a
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-07-07 17:18:54 +0700

    Fix issue

--- views/superuser/coupon/login.php
+++
@@ -1,9 +0,0 @@
-<section>
-  <div class="card">
-    <div class="card-body">
-      <a href="/user/login">Login page (Click Here)</a>
-      <br>
-      <small class="text-muted">Now Migrated into major system</small>
-    </div>
-  </div>
-</section>
