48930e746dd9f4880ca669e7164b39b026fae257
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-07-07 17:13:46 +0700

    Fix shimmer

--- src/shim/autoload.php
+++ src/shim/autoload.php
@@ -1,4 +1,9 @@
 <?php
 foreach (glob(__DIR__ . "/*.php") as $filename) {
-  include $filename;
+  //include $filename;
+  if ($filename = realpath($filename)) {
+    if (basename($filename) != basename(__FILE__)) {
+      echo $filename . PHP_EOL;
+    }
+  }
 }
