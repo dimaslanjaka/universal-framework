587288d3ab2bef339c1e15b9172eb31543872758
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-07-12 22:45:28 +0700

    Fix re-index

--- etc/superuser/breadcrumb.php
+++ etc/superuser/breadcrumb.php
@@ -1,10 +1,19 @@
 <nav aria-label="breadcrumb" class="">
   <ol class="breadcrumb white">
     <li class="breadcrumb-item"><a href="/superuser/"><i class="fad fa-home"></i></a></li>
-    <li class="breadcrumb-item"><a href="/superuser/coupon">Voucher</a></li>
-    <li class="breadcrumb-item"><a href="/superuser/theme">Themes</a></li>
-    <li class="breadcrumb-item"><a href="/superuser/users">Users</a></li>
-    <li class="breadcrumb-item"><a href="/superuser/php">PHP</a></li>
-    <li class="breadcrumb-item"><a href="/superuser/npm">NPM</a></li>
+    <?php
+    $dirs = array_filter(glob(__DIR__ . '/*'), 'is_dir');
+    if (is_array($dirs)) {
+      $map = array_values(array_unique(array_filter(array_map('basename', $dirs))));
+      for ($i = 0; $i < count($map); $i++) {
+        if ($i == count($map) - 1) {
+          //latest iteration
+          echo '<li class="breadcrumb-item active"><a href="/superuser/' . $map[$i] . '/">' . strtoupper($map[$i]) . '</a></li>';
+        } else {
+          echo '<li class="breadcrumb-item"><a href="/superuser/' . $map[$i] . '/">' . strtoupper($map[$i]) . '</a></li>';
+        }
+      }
+    }
+    ?>
   </ol>
 </nav>
--- etc/superuser/users/index-f.php
+++
@@ -1,8 +0,0 @@
-<?php
-user()->admin_required('/user/login');
-
-if (isset($_REQUEST['change']) && 'max' == $_REQUEST['change']) {
-  $res = addLimitMax($_REQUEST['user_id'], $_REQUEST['value']);
-  e($res);
-  exit;
-}
