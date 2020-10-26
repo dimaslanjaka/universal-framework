87bb442e9a97408cc8280319c52331dbb3a16214
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-10-26 17:49:24 +0700

    Inherited from original repository for now

--- views/test/server.js
+++ views/test/server.js
@@ -1,26 +1,25 @@
 (function () {
-  /*
-  $.ajax({
-    url: location.href,
-    method: "POST",
-    complete: function (res) {
-      $("pre#server").html(JSON.stringify(res));
+  const requireJquery = require.config({
+    context: "jquery",
+    baseUrl: "/node_modules/jquery",
+    paths: {
+      jquery: "/node_modules/jquery/src",
+      sizzle: "/node_modules/jquery/external/sizzle/dist/sizzle",
     },
   });
-  */
-})();
-const requireJquery = require.config({
-  context: "jquery",
-  baseUrl: "/node_modules/jquery",
-  paths: {
-    jquery: "/node_modules/jquery/src",
-    sizzle: "/node_modules/jquery/external/sizzle/dist/sizzle",
-  },
-});
 
-requireJquery(["jquery/ajax", "jquery/ajax/xhr"], function ($) {
-  $.ajax({
-    url: "https://httpbin.org/anything",
-    global: false,
+  requireJquery(["jquery/ajax", "jquery/ajax/xhr"], function ($) {
+    $.ajax({
+      url: "https://httpbin.org/anything",
+      global: false,
+    });
+
+    $.ajax({
+      url: location.href,
+      method: "POST",
+      complete: function (res) {
+        $("pre#server").html(JSON.stringify(res, null, 4));
+      },
+    });
   });
-});
+})();
