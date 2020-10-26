f8a6114a08ed2365a5af80d11db9b51ebf5ff8fa
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-10-27 03:33:24 +0700

    remove bad refresh

--- refresh.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-rem %mydate%_%mytime%
-git rm -r --cached .
-git add .
-git commit -m "clear git cache (%mydate% %mytime%)"
-git push
