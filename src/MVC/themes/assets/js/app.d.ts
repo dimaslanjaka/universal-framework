5a040813d11271d72e57e911dd4d0315ac7fec9a
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-11-18 06:57:44 +0700

    Fix update

--- src/MVC/themes/assets/js/app.d.ts
+++ src/MVC/themes/assets/js/app.d.ts
@@ -925,17 +925,17 @@ declare function calculatorDistance(elem: JQuery, mouseX: number, mouseY: number
  */
 declare function prepEntities(str: string): string;
 declare var entityMap: {
-    "160": string;
-    "161": string;
-    "162": string;
-    "163": string;
-    "164": string;
-    "165": string;
-    "166": string;
-    "167": string;
-    "168": string;
-    "169": string;
-    "8364": string;
+    160: string;
+    161: string;
+    162: string;
+    163: string;
+    164: string;
+    165: string;
+    166: string;
+    167: string;
+    168: string;
+    169: string;
+    8364: string;
 };
 /**
  * php equivalent http_build_query
@@ -1269,20 +1269,57 @@ declare function recaptcha(): {
      */
     reCaptcha_buttons: (reCaptcha_disable: boolean, callback: Function) => void;
 };
-declare namespace reCaptcha {
-    const gexec_count: number;
-    const key: string;
-    function js(url: string, callback: Function): void;
-    function set_key(key: string): void;
-    function start(): void;
-    function init(): void;
-    const retry_count: number;
-    function exec(action: any, retry: any, callback: any): void;
-    function insert(token: string): void;
-    function distribute_token(token: string): void;
-    function get(): string;
-    function reCaptcha_buttons(reCaptcha_disable: boolean, callback: Function): void;
-}
+declare var reCaptcha: {
+    /**
+     * @type {Number} counter executions
+     */
+    gexec_count: number;
+    key: string;
+    /**
+     * Javascript caller
+     * @param {String} url
+     * @param {Function} callback
+     */
+    js: (url: string, callback: Function) => void;
+    /**
+     * Set recaptcha site key
+     * @param {String} key
+     */
+    set_key: (key: string) => void;
+    /**
+     * Start recaptcha
+     */
+    start: () => void;
+    /**
+     * Initialize Recaptcha by defining jquery
+     */
+    init: () => void;
+    retry_count: number;
+    /**
+     * load or refreshing google recaptcha
+     */
+    exec: (action: any, retry: any, callback: any) => void;
+    /**
+     * Insert reCaptcha Token
+     * @param {String} token
+     */
+    insert: (token: string) => void;
+    /**
+     * Distribute reCaptcha Token
+     * @param {String} token
+     */
+    distribute_token: (token: string) => void;
+    /**
+     * Get token recaptcha
+     */
+    get: () => string;
+    /**
+     * Button Controller
+     * @param {Boolean} reCaptcha_disable
+     * @param {Function} callback
+     */
+    reCaptcha_buttons: (reCaptcha_disable: boolean, callback: Function) => void;
+};
 declare const requirejs_vendor = "/node_modules";
 declare const require_config: RequireConfig;
 interface RequireConfig {
@@ -1525,4 +1562,3 @@ declare class ZLIB {
     static decompress(str: any): string;
     static compress(str: any): any;
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
