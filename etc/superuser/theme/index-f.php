59a058bb42b5a34c8e186e309e3b85a7bb3aac75
dimaslanjaka <dimaslanjaka@gmail.com>
2021-05-24 05:03:43 +0700

    X

--- etc/superuser/theme/index-f.php
+++ etc/superuser/theme/index-f.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   if (isset($_POST['save_config'])) {
     unset($_POST['save_config']);
     $config = \JSON\json::init()->load(ROOT . '/config.json', true);
-    $config->result['cache']['ext'] = $_POST['cache']['ext'];
+    //$config->result['cache']['ext'] = $_POST['cache']['ext'];
     $config->save();
   }
 }
