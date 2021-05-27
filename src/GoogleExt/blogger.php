eafce050916d7370d88f1eb5f81712b9b3c5b3a0
dimaslanjaka <dimaslanjaka@gmail.com>
2021-05-27 09:04:55 +0700

    Fix blogger class

--- src/GoogleExt/blogger.php
+++ src/GoogleExt/blogger.php
@@ -1,4 +1,6 @@
-<?php /** @noinspection PhpSameParameterValueInspection */
+<?php
+
+/** @noinspection PhpSameParameterValueInspection */
 
 namespace GoogleExt;
 
@@ -10,119 +12,113 @@ use Google_Service_Blogger_Blog;
 
 class blogger
 {
-    public $posts;
-    public $blogs;
-    public $blogId;
-    private $service;
-    private $result_get_post = [];
-<<<<<<< HEAD
-    private $configFolder = __DIR__ . '/config';
-    /**
-     * @var bool Recrawl cache curl
-     */
-    public $recrawl = false;
-=======
->>>>>>> b6599a978a9b1fe18ebb02b6631e32ab66c795f7
+  public $posts;
+  public $blogs;
+  public $blogId;
+  private $service;
+  private $result_get_post = [];
+  private $configFolder = __DIR__ . '/config';
+  /**
+   * @var bool Recrawl cache curl
+   */
+  public $recrawl = false;
 
-    /**
-     * @param Google_Client|client $client
-     */
-    public function __construct($client)
-    {
-        $this->service = new Google_Service_Blogger($client);
-        $this->blogs = $this->service->blogs;
-        $this->posts = $this->service->posts;
+  /**
+   * @param Google_Client|client $client
+   */
+  public function __construct($client)
+  {
+    $this->service = new Google_Service_Blogger($client);
+    $this->blogs = $this->service->blogs;
+    $this->posts = $this->service->posts;
+  }
+
+  /**
+   * Set Blog By URL.
+   *
+   * @param $url
+   *
+   * @return Google_Service_Blogger_Blog
+   */
+  public function byUrl($url)
+  {
+    $blog = $this->service->blogs->getByUrl($url);
+    $this->blogId = $blog->getId();
+
+    return $blog;
+  }
+
+  public function listPosts($limit = 5)
+  {
+    return $this->posts->listPosts($this->blogId, ['maxResults' => $limit]);
+  }
+
+  public function setBlogId($id)
+  {
+    $this->blogId = $id;
+  }
+
+  /**
+   * Get All Blogger Posts.
+   *
+   * @param string|null $blogId
+   * @param string|null $pageToken
+   *
+   * @return array
+   */
+  public function getPosts($blogId = null, $pageToken = null)
+  {
+    if (null == $blogId) {
+      $blogId = $this->blogId;
+    }
+    $query = [
+      'key' => CONFIG['google']['key'],
+    ];
+    if (is_string($pageToken) && !empty($pageToken)) {
+      $query['pageToken'] = $pageToken;
+    }
+    $url = "https://www.googleapis.com/blogger/v3/blogs/$blogId/posts?" . http_build_query($query);
+    $response = $this->curl($url);
+    if (isset($response['items'])) {
+      $this->result_get_post = array_merge($this->result_get_post, $response['items']);
+    }
+    if (isset($response['nextPageToken'])) {
+      return $this->getPosts($blogId, $response['nextPageToken']);
     }
 
-    /**
-     * Set Blog By URL
-     * @param $url
-     * @return Google_Service_Blogger_Blog
-     */
-    public function byUrl($url)
-    {
-        $blog = $this->service->blogs->getByUrl($url);
-        $this->blogId = $blog->getId();
+    return $this->result_get_post;
+  }
 
-        return $blog;
+  /**
+   * Blogger Curl.
+   *
+   * @param $url
+   *
+   * @return array
+   */
+  private function curl($url)
+  {
+    $cacheId = md5($url);
+    $cacheFile = ROOT . '/tmp/curl/' . __CLASS__ . '/' . __FUNCTION__ . '/' . $cacheId;
+    if (file_exists($cacheFile) && !$this->recrawl) {
+      //$cacheModified = filemtime($cacheFile);
+      //$minute = floor((time() - $cacheModified) / (60));
+      //echo $minute . PHP_EOL;
+      return json_decode(file_get_contents($cacheFile), true);
     }
 
-    public function listPosts($limit = 5)
-    {
-        return $this->posts->listPosts($this->blogId, ['maxResults' => $limit]);
+    try {
+      $ch = curl_init($url);
+
+      curl_setopt($ch, CURLOPT_HTTPGET, 1);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      $resp = curl_exec($ch);
+      file::write($cacheFile, $resp);
+      curl_close($ch);
+
+      return json_decode($resp, true);
+    } catch (Exception $ex) {
+      return ['error' => true, 'message' => $ex->getMessage()];
     }
-
-    public function setBlogId($id)
-    {
-        $this->blogId = $id;
-    }
-
-    /**
-     * Get All Blogger Posts
-     * @param null|string $blogId
-     * @param null|string $pageToken
-     * @return array
-     */
-    public function getPosts($blogId = null, $pageToken = null)
-    {
-        if ($blogId == null) {
-            $blogId = $this->blogId;
-        }
-        $query = [
-            'key' => CONFIG['google']['key']
-        ];
-        if (is_string($pageToken) && !empty($pageToken)) {
-            $query['pageToken'] = $pageToken;
-        }
-        $url = "https://www.googleapis.com/blogger/v3/blogs/$blogId/posts?" . http_build_query($query);
-        $response = $this->curl($url);
-        if (isset($response['items'])) {
-            $this->result_get_post = array_merge($this->result_get_post, $response['items']);
-        }
-        if (isset($response['nextPageToken'])) {
-            return $this->getPosts($blogId, $response['nextPageToken']);
-        }
-        return $this->result_get_post;
-    }
-
-    /**
-     * Blogger Curl
-     * @param $url
-<<<<<<< HEAD
-     * @return array
-     */
-    private function curl($url)
-    {
-        $cacheId = md5($url);
-        $cacheFile = ROOT . '/tmp/curl/' . __CLASS__ . '/' . __FUNCTION__ . '/' . $cacheId;
-        if (file_exists($cacheFile) && !$this->recrawl) {
-=======
-     * @param bool $recrawl
-     * @return array
-     */
-    private function curl($url, $recrawl = false)
-    {
-        $cacheId = md5($url);
-        $cacheFile = ROOT . '/tmp/curl/' . __CLASS__ . '/' . __FUNCTION__ . '/' . $cacheId;
-        if (file_exists($cacheFile) && !$recrawl) {
->>>>>>> b6599a978a9b1fe18ebb02b6631e32ab66c795f7
-            //$cacheModified = filemtime($cacheFile);
-            //$minute = floor((time() - $cacheModified) / (60));
-            //echo $minute . PHP_EOL;
-            return json_decode(file_get_contents($cacheFile), true);
-        }
-
-        try {
-            $ch = curl_init($url);
-
-            curl_setopt($ch, CURLOPT_HTTPGET, 1);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            $resp = curl_exec($ch);
-            file::write($cacheFile, $resp);
-            curl_close($ch);
-            return json_decode($resp, true);
-        } catch (Exception $ex) {
-            return ['error' => true, 'message' => $ex->getMessage()];
-        }
-    }
+  }
 }
